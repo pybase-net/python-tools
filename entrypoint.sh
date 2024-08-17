3fe3af4a9481d93eb8d7de3e5a4c61dbb16118cf
misostack <misostack.com@gmail.com>
2024-08-17 21:00:15 +0700

    run worker and beat only

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@
 #sleep 5
 
 # Start Celery worker and beat
-flask run --host=0.0.0.0 --port=8080 &
+# flask run --host=0.0.0.0 --port=8080 &
 celery -A make_celery.celery_app worker -B --loglevel=info --concurrency=1
